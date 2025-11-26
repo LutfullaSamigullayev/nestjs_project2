2ad0fdb6474212b433bb5275c5202cb8708e3c38
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-26 18:10:29 +0500

    register complate

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,9 +2,18 @@ import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/sequelize";
 import { User } from "./schema/user.schema";
 import { CreateUserDto } from "./dto/create-user.dto";
+import * as bcrypt from 'bcrypt';
+import nodemailer from 'nodemailer'
 
 @Injectable()
 export class AuthService {
+  private transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "samigullayevlutfulla@gmail.com",
+      pass: process.env.APP_PASSWORD as string
+    }
+  })
   constructor(@InjectModel(User) private userModel: typeof User) {}
 
   async register(createUserDto: CreateUserDto): Promise<{ message: string }> {
@@ -15,8 +24,20 @@ export class AuthService {
     if(foundedUser) {
         throw new UnauthorizedException("User is exits")
     }
-    const hash = 
-    this.userModel.create({createUserDto})
+
+    const hash = await bcrypt.hash(password, 10);
+    const randomNumber = Array.from({length: 6}, () => Math.floor(Math.random() * 10)).join('')
+
+    await this.transporter.sendMail({
+      from: "samigullayevlutfulla@gmail.com",
+      to: email,
+      subject: 'Nestjs project',
+      text: `${randomNumber}`
+    })
+    
+    const time = Date.now()
+
+    await this.userModel.create({username, email, password: hash, otp: randomNumber, otpTime: (time + 120000)})
 
     return { message: "Registered" };
   }
