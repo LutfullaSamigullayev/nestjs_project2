469064b2404a5a80590114bf1cfdde1513097e7d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-26 14:21:29 +0500

    register service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { User } from "./schema/user.schema";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectModel(User) private userModel: typeof User) {}
+
+  async register(createUserDto: CreateUserDto): Promise<{ message: string }> {
+    const { username, email, password } = createUserDto;
+
+    const foundedUser = await this.userModel.findOne({where: {email}})
+
+    if(foundedUser) {
+        throw new UnauthorizedException("User is exits")
+    }
+    const hash = 
+    this.userModel.create({createUserDto})
+
+    return { message: "Registered" };
+  }
+}
