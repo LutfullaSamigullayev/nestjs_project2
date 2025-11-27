56b0edef4621f65c727d0e10bea355c3ef90dbf8
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-27 22:48:59 +0500

    update db orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: ".env", isGlobal: true}),
-    SequelizeModule.forRoot({
-      dialect: "postgres",
+  TypeOrmModule.forRoot({
+      type: "postgres",
       username: "postgres",
       port: 5432,
       password: String(process.env.DB_PASSWORD as string),
       database: String(process.env.DB_DATABASE as string),
       synchronize: true,
-      autoLoadModels: true,
+      entities: [],
       logging: false
     }),
     AuthModule
