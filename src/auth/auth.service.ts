5ccea477088c2095f1b01969ffb95bde147bc234
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-28 17:02:17 +0500

    update login service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
-import { BadRequestException, Injectable, UnauthorizedException } from "@nestjs/common";
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { User } from "./entities/user.entity";
 import { CreateUserDto, LoginDto, VerifyDto } from "./dto/create-user.dto";
-import * as bcrypt from 'bcrypt';
-import nodemailer from 'nodemailer'
-import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from "bcrypt";
+import nodemailer from "nodemailer";
+import { JwtService } from "@nestjs/jwt";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 
@@ -13,9 +17,9 @@ export class AuthService {
     service: "gmail",
     auth: {
       user: "samigullayevlutfulla@gmail.com",
-      pass: process.env.APP_PASSWORD as string
-    }
-  })
+      pass: process.env.APP_PASSWORD as string,
+    },
+  });
   constructor(
     @InjectRepository(User) private userRepo: Repository<User>,
     private jwtService: JwtService
@@ -24,27 +28,35 @@ export class AuthService {
   async register(createUserDto: CreateUserDto): Promise<{ message: string }> {
     const { username, email, password } = createUserDto;
 
-    const foundedUser = await this.userRepo.findOne({where: {email}})
+    const foundedUser = await this.userRepo.findOne({ where: { email } });
 
-    if(foundedUser) {
-        throw new UnauthorizedException("User is exits")
+    if (foundedUser) {
+      throw new UnauthorizedException("User is exits");
     }
 
     const hash = await bcrypt.hash(password, 10);
-    const randomNumber = Array.from({length: 6}, () => Math.floor(Math.random() * 10)).join('')
+    const randomNumber = Array.from({ length: 6 }, () =>
+      Math.floor(Math.random() * 10)
+    ).join("");
 
     await this.transporter.sendMail({
       from: "samigullayevlutfulla@gmail.com",
       to: email,
-      subject: 'Nestjs project',
-      text: `${randomNumber}`
-    })
+      subject: "Nestjs project",
+      text: `${randomNumber}`,
+    });
 
-    const time = Date.now() + 120000
+    const time = Date.now() + 120000;
 
-    const newUser = this.userRepo.create({username, email, password: hash, otp: randomNumber, otpTime: time})
+    const newUser = this.userRepo.create({
+      username,
+      email,
+      password: hash,
+      otp: randomNumber,
+      otpTime: time,
+    });
 
-    await this.userRepo.save(newUser)
+    await this.userRepo.save(newUser);
 
     return { message: "Registered" };
   }
@@ -52,22 +64,27 @@ export class AuthService {
   async verify(verifyDto: VerifyDto): Promise<{ message: string }> {
     const { email, otp } = verifyDto;
 
-    const foundedUser = await this.userRepo.findOne({where: {email}})
+    const foundedUser = await this.userRepo.findOne({ where: { email } });
 
-    if(!foundedUser) {
-        throw new UnauthorizedException("User not found")
-    }
-    
-    const time = Date.now()
-
-    if(foundedUser.otpTime < time) {
-      throw new BadRequestException("Otp time expired")
+    if (!foundedUser) {
+      throw new UnauthorizedException("User not found");
     }
 
-    if(foundedUser.otp === otp) {
-      await this.userRepo.update({otp: null, otpTime: null, isVerify: true}, {where: {email}})
+    const time = Date.now();
+    if (foundedUser.otpTime) {
+      if (foundedUser.otpTime < time) {
+        throw new BadRequestException("Otp time expired");
+      }
+    }
+
+    if (foundedUser.otp === otp) {
+      await this.userRepo.update(email, {
+        otp: null,
+        otpTime: null,
+        isVerify: true,
+      });
     } else {
-      throw new BadRequestException("Wrong otp")
+      throw new BadRequestException("Wrong otp");
     }
 
     return { message: "Verify" };
@@ -75,29 +92,29 @@ export class AuthService {
 
   async login(loginDto: LoginDto): Promise<{ access_token: string }> {
     const { email, password } = loginDto;
-    const foundedUser = await this.userRepo.findOne({where: {email}})
+    const foundedUser = await this.userRepo.findOne({ where: { email } });
 
-    if(!foundedUser) {
-        throw new UnauthorizedException("User not found")
+    if (!foundedUser) {
+      throw new UnauthorizedException("User not found");
     }
 
-    const decode = await bcrypt.compare(password, foundedUser.password)
-    if(decode && foundedUser.isVerify) {
+    const decode = await bcrypt.compare(password, foundedUser.password);
+    if (decode && foundedUser.isVerify) {
       const payload = { sub: foundedUser.id, username: foundedUser.username };
-      return {access_token: await this.jwtService.signAsync(payload)}
-    }else {
-      throw new BadRequestException("Invalid password")
+      return { access_token: await this.jwtService.signAsync(payload) };
+    } else {
+      throw new BadRequestException("Invalid password");
     }
   }
 
-  async deleteUser (id: number): Promise<boolean> {
-    const foundedUser = await this.userRepo.findOne({where: {id}})
+  async deleteUser(id: number): Promise<boolean> {
+    const foundedUser = await this.userRepo.findOne({ where: { id } });
 
-    if(!foundedUser) {
-        throw new UnauthorizedException("User not found")
+    if (!foundedUser) {
+      throw new UnauthorizedException("User not found");
     }
-    await this.userRepo.delete({id})
+    await this.userRepo.delete({ id });
 
-    return true
+    return true;
   }
 }
