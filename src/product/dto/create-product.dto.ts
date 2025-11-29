e49d11ef6c55f4c6cccf85a169b0ba5648b2cd96
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-29 09:16:37 +0500

    added product dto validation

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,16 @@
+import { IsNumber, IsString, Length } from "class-validator";
+
 export class CreateProductDto {
+  @IsString()
   title: string;
-  price: string;
+
+  @IsNumber()
+  price: number;
+
+  @IsString()
   img?: string;
+
+  @IsString()
+  @Length(5, 1000)
   desc?: string;
 }
