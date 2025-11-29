110681a421f0c1fde3866261199ad07fd476ef2b
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-29 09:16:18 +0500

    added user dto validation

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,15 +1,32 @@
+import { IsEmail, IsString, Length, MinLength } from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
+    @MinLength(3)
     username: string;
+
+    @IsEmail()
     email: string;
+
+    @IsString()
+    @Length(8, 50)
     password: string;
 }
 
 export class VerifyDto {
+    @IsEmail()
     email: string;
+
+    @IsString()
+    @Length(6, 6)
     otp: string;
 }
 
 export class LoginDto {
+    @IsEmail()
     email: string;
+
+    @IsString()
+    @Length(8, 50)
     password: string;
 }
