7b24304a44633b7144d24cf7a6642dfc3e14f869
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-30 21:31:21 +0500

    import filter main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { CustomLogger } from './common/logger/custom.logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +14,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   )
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000, () => {
     console.log('Server ishladi: ', process.env.PORT);
