8db4601fbf8da3b6f2309e75525fa10f55df9023
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-30 21:42:24 +0500

    auth swagger complate

--- src/module/auth/dto/create-user.dto.ts
+++ src/module/auth/dto/create-user.dto.ts
@@ -1,31 +1,39 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, Length, MinLength } from "class-validator";
 
 export class CreateUserDto {
+    @ApiProperty({description: "about username", default: "ali"})
     @IsString()
     @MinLength(3)
     username: string;
 
+    @ApiProperty({default: "samigullayevlutfulla@gmail.com"})
     @IsEmail()
     email: string;
 
+    @ApiProperty({default: "12345678"})
     @IsString()
     @Length(8, 50)
     password: string;
 }
 
 export class VerifyDto {
+    @ApiProperty({default: "samigullayevlutfulla@gmail.com"})
     @IsEmail()
     email: string;
 
+    @ApiProperty()
     @IsString()
     @Length(6, 6)
     otp: string;
 }
 
 export class LoginDto {
+    @ApiProperty({default: "samigullayevlutfulla@gmail.com"})
     @IsEmail()
     email: string;
 
+    @ApiProperty({default: "12345678"})
     @IsString()
     @Length(8, 50)
     password: string;
