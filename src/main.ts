69e6114b4814babdf477c312f6e500ac942e49ba
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-30 21:40:02 +0500

    connect main.ts swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { CustomLogger } from './common/logger/custom.logger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,31 @@ async function bootstrap() {
   )
 
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle("Lesson")
+    .setDescription("The lesson API description")
+    .setVersion("1.0")
+    .addTag("lesson")
+    .addGlobalResponse({
+      status: 500,
+      description: "Internal server error",
+    })
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth', // Bu nom guardlarda ishlatiladi
+    )
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api-docs", app, documentFactory);
 
   app.useLogger(app.get(CustomLogger));
 
