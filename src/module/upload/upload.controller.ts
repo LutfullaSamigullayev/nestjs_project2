1f89ed4069b1d5a0fadb18654882116d4ff2ea44
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-02 14:55:46 +0500

    added multiple upload

--- src/module/upload/upload.controller.ts
+++ src/module/upload/upload.controller.ts
@@ -1,23 +1,49 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
-import { FileInterceptor } from "@nestjs/platform-express";
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from "@nestjs/common";
+import { FileInterceptor, FilesInterceptor } from "@nestjs/platform-express";
 import { diskStorage } from "multer";
 import path from "path";
 
 @Controller("upload")
 export class UploadController {
+  // single
   @Post("single")
   @UseInterceptors(
     FileInterceptor("file", {
       storage: diskStorage({
         destination: path.join(process.cwd(), "uploads"),
-        filename: function(req, file, cb) {
-            const uniquePerfix = `${file.fieldname}${Date.now()}${path.extname(file.originalname)}`
-            cb(null, uniquePerfix)
-        }
+        filename: function (req, file, cb) {
+          const uniquePerfix = `${file.fieldname}${Date.now()}${path.extname(file.originalname)}`;
+          cb(null, uniquePerfix);
+        },
       }),
     })
   )
   singleFileUpload(@UploadedFile() file: Express.Multer.File) {
-    return {file_path: `http://localhost:4001/uploads/${file.fieldname}`}
+    return { file_path: `http://localhost:4001/uploads/${file.filename}` };
+  }
+
+  // multiple
+  @Post("multiple")
+  @UseInterceptors(
+    FilesInterceptor("files", 10, {
+      storage: diskStorage({
+        destination: path.join(process.cwd(), "uploads"),
+        filename: function (req, file, cb) {
+          const uniquePerfix = `${file.fieldname}${Date.now()}${path.extname(file.originalname)}`;
+          cb(null, uniquePerfix);
+        },
+      }),
+    })
+  )
+  multipleFileUpload(@UploadedFiles() files: Express.Multer.File[]) {
+    return files.map(
+      (file) => `http://localhost:4001/uploads/${file.filename}`
+    );
   }
 }
