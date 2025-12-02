66898303e37011b40bb840b7736e8fa842a0a572
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-02 14:31:47 +0500

    added single upload controller

--- src/module/upload/upload.controller.ts
+++ src/module/upload/upload.controller.ts
@@ -1,6 +1,23 @@
-import { Controller} from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import path from "path";
 
-@Controller('upload')
+@Controller("upload")
 export class UploadController {
-  
+  @Post("single")
+  @UseInterceptors(
+    FileInterceptor("file", {
+      storage: diskStorage({
+        destination: path.join(process.cwd(), "uploads"),
+        filename: function(req, file, cb) {
+            const uniquePerfix = `${file.fieldname}${Date.now()}${path.extname(file.originalname)}`
+            cb(null, uniquePerfix)
+        }
+      }),
+    })
+  )
+  singleFileUpload(@UploadedFile() file: Express.Multer.File) {
+    return {file_path: `http://localhost:4001/uploads/${file.fieldname}`}
+  }
 }
